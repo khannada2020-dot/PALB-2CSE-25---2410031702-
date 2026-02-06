ff5da806dd5d159f35cdda95164424be66d4208c
khannada2020-dot <khannada2020@gmail.com>
2026-02-06 20:37:54 +0530

    added all files

---
+++ Homework Assignment/04_program.py
@@ -0,0 +1,5 @@
+a = [1, 2, 3, 2, 1]
+b = [3, 2, 2, 3, 3, 2]
+
+union = set(a).union(set(b))
+print(list(union))
---
+++ Homework Assignment/05_program.py
@@ -0,0 +1,8 @@
+arr = [1, 8, 7, 56, 90]
+
+largest = arr[0]
+for i in arr:
+    if i > largest:
+        largest = i
+
+print(largest)
